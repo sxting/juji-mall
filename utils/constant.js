5e04bfc7dda7f97816062a55ff2d1bf4bc52406a
webskating <494945016@qq.com>
2019-01-18 14:06:22 +0800

    111

--- utils/constant.js
+++ utils/constant.js
@@ -1,7 +1,7 @@
 var constant = {
   TPLID: '1.0.0',//应用版本
-  APPID: 'wx2f60fa7a673db8be',//小程序wxid
-  apiUrl: 'https://boss.juniuo.com',//接口前缀
+  APPID: 'wxb03b0ab93bb60ecc',//小程序wxid
+  apiUrl: 'https://juji-dev.juniuo.com',//接口前缀
   basePicUrl: 'https://upic.juniuo.com/file/picture/',//图片前缀
   basePicFix: '/resize_320_0/mode_fill',//图片后缀
   basePicFix1: '/resize_80_0/mode_fill'//图片后缀

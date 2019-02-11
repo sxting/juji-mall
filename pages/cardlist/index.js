2119de78fcbafd0046abad6cfdc09d2393f99fd4
webskating <494945016@qq.com>
2019-02-11 21:09:41 +0800

    登录逻辑修改

--- pages/cardlist/index.js
+++ pages/cardlist/index.js
@@ -1,85 +1,66 @@
-import { errDialog, loading } from '../../utils/util';
-import { constant } from '../../utils/constant';
-import { service } from '../../service';
+// pages/cardlist/index.js
 Page({
+
+  /**
+   * 页面的初始数据
+   */
   data: {
-  	tablist:['全部','美食休闲','丽人/美发'],
-  	curTabIndex:0,
-    isShowNodata:false,
-    curMerchantType:0,
-    curCardIndex:-1,
-    isShow:false,
-    cardListAll:[],
-    cardList:[]
+
   },
-  toBalanceSrc: function(event){
-    var index = event.currentTarget.dataset['index'];
-    var balanceSrcArr = JSON.stringify(this.data.cardList[index].list);
-    var cardType = this.data.cardList[index].cardType;
-    wx.setStorageSync('balanceSrcArr',balanceSrcArr);
-    wx.navigateTo({url: '/pages/balanceSrc/index?type='+cardType});
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
   },
-  useCard:function(e){
-    var id=e.currentTarget.dataset.id;
-    var cardid=e.currentTarget.dataset.cardid;
-    if(e.currentTarget.dataset.type==1){
-      wx.navigateTo({url: '/pages/createOrder/index?id='+id+'&cardid='+cardid});
-    }else{
-      wx.navigateTo({url: '/pages/setupOrder/index?id='+id+'&cardid='+cardid});
-    }
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
   },
-  toMerchant:function(e){
-    var id=e.currentTarget.dataset.id;
-    var type=e.currentTarget.dataset.type;
-    wx.navigateTo({url: '/pages/businessDetails/index?id='+id+'&cardType='+type});
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
   },
-  getCardList: function(){
-    service.listCardPackage().subscribe({
-      next: res => {
-        this.setData({cardListAll:res});
-        this.setData({cardList:res});
-        this.setData({isShowNodata:this.data.cardList.length==0});
-      },
-      error: err => errDialog(err),
-      complete: () => wx.hideToast()
-    })
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
   },
-  slideThisItem:function(e){
-    var thisIndex=e.currentTarget.dataset['index'];
-    if(thisIndex==this.data.curCardIndex){
-      this.setData({curCardIndex:-1});
-    }else{
-      this.setData({curCardIndex:thisIndex});
-    }
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
   },
-  switchTab: function(event){
-    var thisIndex=event.currentTarget.dataset['index'];
-    var thisType=event.currentTarget.dataset['type'];
-    this.setData({curTabIndex:thisIndex});
-    if(thisType==0){
-      this.setData({cardList:this.data.cardListAll});
-    }else{
-      this.setData({cardList:this.getCardListByType(thisType)});
-    }
-    this.setData({isShowNodata:this.data.cardList.length==0});
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
   },
-  getCardListByType: function(type){
-    var cardArr = this.data.cardListAll;
-    var newCardList = [];
-    for(var i=0;i<cardArr.length;i++){
-      if(cardArr[i].merchantType==type){
-        newCardList.push(cardArr[i]);
-      }
-    }
-    return newCardList;
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
   },
-  toMyCard: function(){
-    wx.navigateTo({url: '/pages/myCard/index'})
-  },
-  onShow: function(){
-    this.getCardList();
-  },
-  onLoad: function(options) {
-    wx.setNavigationBarTitle({title: '卡包'});
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
   }
-});
+})

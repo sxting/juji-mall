a29c5749037495de62063f128ef5866fadce15dc
webskating <494945016@qq.com>
2019-03-04 12:00:26 +0800

    不存在rowData弹出问题

--- pages/jujipay/index.js
+++ pages/jujipay/index.js
@@ -10,7 +10,11 @@ Page({
     showFocus: true,//显示光标
     numArr: [1, 2, 3, 4, 5, 6, 7, 8, 9],
     amount: '',
-    maxAmount: 20000
+    maxAmount: 20000,
+    seledAccount:false,
+    showBalanceWrap: false,
+    showSelectCard: false,
+    cardList: [{},{},{}],
   },
 
   /**
@@ -154,6 +158,93 @@ Page({
     this.setData({
       amount: content
     });
+  },
+
+  toResult() {
+    if (this.data.amount != '' && this.data.amount != '请输入付款金额' && this.data.amount != 0) {
+
+      if (this.data.seledAccount) {//如果用户勾选了余额支付
+        //此处判断余额付款和组合付款的情况 余额付款直接跳转到结果页面 组合支付还要到当前页的另一层
+        if (Number(this.amount) > this.balance) {//如果输入金额比用户余额多
+          this.dAmount = Number(this.amount).toFixed(2);//保存一个接口传入需要用的amount
+          this.amount = Number(Number(this.amount) - this.balance).toFixed(2);
+          this.getPayment(this.dAmount);
+          this.showBalanceWrap = false;
+          this.showFocus = false;
+          this.showSelectCard = true;
+        } else {//如果用户余额充足，可以执行余额支付
+          this.accountpaystatus = true;
+          let orderObj = {};
+          this.paytype ? orderObj.choosenType = this.paytype : orderObj;
+          orderObj.orderPay = Number(this.amount);
+          orderObj.pay = Number(this.amount);
+          this.accountPay(orderObj);
+        }
+      } else {
+        //如果没有勾选 
+        this.setData({
+          amount: Number(this.data.amount).toFixed(2),
+          dAmount: Number(this.data.amount).toFixed(2)
+        });
+        // this.getPayment(this.dAmount);
+        this.setData({
+          showBalanceWrap: false,
+          showFocus: false,
+          showSelectCard: true
+        });
+      }
+    }
+  },
+
+  getPayment(orderPay) {
+    axios({
+      url: '/customer/order/getPayment.json',
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Token': $$('accessToken').value
+      },
+      params: {
+        storeId: $$('storeId').value,
+        orderPay: orderPay
+      }
+    }).then(res => {
+      console.log('----------余额以及卡列表数据等----------');
+      console.log(res);
+      if (res.data.errorCode == "0") {
+        //支付类型不再依赖于返回 如果 this.cardList长度>0则默认 recommend 没有选择余额时为 thirdpay
+        this.cardList = res.data.data.recommendCards;
+        if (this.cardList.length > 0) {
+          if ($$('recommend').value == '1') {
+            this.paytype = 'recommend';
+            this.changeSelectCard(this.cardList[0], 0);
+          } else {
+            if (this.seledAccount) {//如果选择余额支付
+              this.paytype = 'account';//组合支付情况
+            } else {
+              this.paytype = 'thirdpay';
+            }
+            this.selectCardId = this.cardList[0].prepayRuleId;
+            this.selectCardPay = Number(this.cardList[0].pay).toFixed(2);
+            this.givingMoney = this.cardList[0].givingMoney;
+            this.discount = this.cardList[0].discount;
+            this.selectedSlideIndex = 0;
+          }
+        } else {
+          if (this.seledAccount) {//如果选择余额支付
+            this.paytype = 'account';//组合支付情况
+          } else {
+            this.paytype = 'thirdpay';
+          }
+        }
+        console.log('paytype: ' + this.paytype);
+        console.log('accountFlag: ' + this.accountFlag);
+      } else {
+        alert('系统错误：' + res.data.errorCode);
+      }
+    }).catch(err => {
+      console.log(err);
+    })
   },
 
   /**

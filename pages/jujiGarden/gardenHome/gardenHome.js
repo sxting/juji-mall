366c8398d98982dab0aeaf8111f429781a38b985
chounan <chounannan@juniuo.com>
2019-03-07 16:59:33 +0800

    add new thing about fenxiao

--- pages/jujiGarden/gardenHome/gardenHome.js
+++ pages/jujiGarden/gardenHome/gardenHome.js
@@ -5,14 +5,29 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    juyuanBgFrist: '/images/juyuan-bg1.png',
+    juyuanBgSecond: '/images/juyuan-bg2.png',
+    juminNumList: [],//队员人数
+    hadNumber: 2
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.data.juminNumList = [];
+    if (this.data.hadNumber > 0) {
+      for (let i = 0; i < this.data.hadNumber; i++) {
+        let list = 'yes';
+        this.data.juminNumList.push(list);
+      }
+      for (let j = 0; j < (10 -  parseInt(this.data.hadNumber)); j++){
+        this.data.juminNumList.push('');
+      }
+    }
+    this.setData({
+      juminNumList: this.data.juminNumList,
+    })
   },
 
   /**
@@ -37,10 +52,14 @@ Page({
   },
 
   /**
-   * 生命周期函数--监听页面卸载
+   * 邀请新人加入桔园
    */
-  onUnload: function () {
-
+  inviteNewMembers: function (res) {
+    return {
+      title: JSON.parse(wx.getStorageSync('userinfo')).nickName + '邀请您桔园结义成为桔长，购物最高可享40%商品返利',
+      path: '',
+      imageUrl: ''
+    }
   },
 
   /**

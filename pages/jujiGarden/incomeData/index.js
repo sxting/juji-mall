a0d8f7a86f4071bcd7d3bb9627b8eab5de0dfd5a
wanghongguo <wanghongguo@juniuo.com>
2019-03-15 17:17:20 +0800

    收入页和收入订单页加交互

--- pages/jujiGarden/incomeData/index.js
+++ pages/jujiGarden/incomeData/index.js
@@ -4,7 +4,7 @@ import { errDialog, loading } from '../../../utils/util';
 var app = getApp();
 Page({
     data: {
-        tablist: [{ name: '本日', type: '1' }, { name: '本周', type: '2' }, { name: '本月', type: '2' }, { name: '累计', type: '2' }],
+        tablist: [{ name: '本日', type: '1' }, { name: '本周', type: '2' }, { name: '本月', type: '3' }, { name: '累计', type: '4' }],
         curTabIndex: 0,
         curActiveIndex:1,
         constant: constant,
@@ -13,48 +13,56 @@ Page({
         status:'',
         isFinall:false,
         amount: 0,
-        sortIndex:1
+        sortIndex1:1,
+        sortIndex2:1,
+        incomeData:{},
+        withdrawData:{},
+        incomelist:{},
+        withdrawlist:{}
     },
     onLoad: function(options) {
         wx.setNavigationBarTitle({ title: '我的收入' });
-        getDataByType('',1)
-    },
-    toPage:function(e){
-      var page = e.currentTarget.dataset.page;
-      wx.navigateTo({ url: page });
+        this.getDataByType('',1);
+        this.getDataByType('SETTLED',1);
     },
     switchTab: function(e) {
         var thisIndex = e.currentTarget.dataset.index;
         this.setData({ curTabIndex: thisIndex});
         var type = e.currentTarget.dataset.type;
         this.getDataByType('',type);
+        this.getDataByType('SETTLED',type);
     },
-    toggleLabel:function(e){
+    toggleLabel1:function(e){
         var index = e.currentTarget.dataset.label;
-        this.setData({ sortIndex: index });
+        this.setData({ sortIndex1: index });
+    },
+    toggleLabel2:function(e){
+        var index = e.currentTarget.dataset.label;
+        this.setData({ sortIndex2: index });
     },
     switchActive:function(e){
         var index = e.currentTarget.dataset.index;
         this.setData({ curActiveIndex: index });
     },
     getDataByType:function(status,type){
-        if(type==1){
-            var startDate = '2019-01-01 00:00:00';
-            var endDate = '2019-01-01 00:00:00';
+        if(type==1){//本日
+            var startDate = getNowDate()+' 00:00:00';
+            var endDate = getNowDate()+' 23:59:59';
         }
-        if(type==2){
-            var startDate = '2019-01-01 00:00:00';
-            var endDate = '2019-01-01 00:00:00';
+        if(type==2){//本周
+            var startDate = getWeekFirstDay()+' 00:00:00';
+            var endDate = getWeekLastDay()+' 23:59:59';
         }
-        if(type==3){
-            var startDate = '2019-01-01 00:00:00';
-            var endDate = '2019-01-01 00:00:00';
+        if(type==3){//本月
+            var startDate = getMonthFirstDay()+' 00:00:00';
+            var endDate = getMonthLastDay()+' 23:59:59';
         }
-        if(type==4){
+        if(type==4){//累计
             var startDate = '2019-01-01 00:00:00';
-            var endDate = '2019-01-01 00:00:00';
+            var endDate = '2100-01-01 00:00:00';
         }
         this.getData(status,startDate,endDate);
+        this.getDigestlist(status,startDate,endDate);
     },
     getData: function(status,startDate,endDate){
         jugardenService.getIncomeInfor({
@@ -63,12 +71,80 @@ Page({
             endDate:endDate
         }).subscribe({
             next: res => {
-                this.setData({
-                    
-                });
+                if(status==""){
+                    this.setData({incomeData:res});
+                }else{
+                    this.setData({withdrawData:res});
+                }
             },
             error: err => errDialog(err),
             complete: () => wx.hideToast()
         })
-    }
+    },
+    getDigestlist:function(status,startDate,endDate){
+        jugardenService.getIncomeOrderDigests({
+            status:status,
+            startDate:startDate,
+            endDate:endDate
+        }).subscribe({
+            next: res => {
+                if(status==""){
+                    this.setData({incomelist:res});
+                }else{
+                    this.setData({withdrawlist:res});
+                }
+            },
+            error: err => errDialog(err),
+            complete: () => wx.hideToast()
+        })
+    },
+    toDetail:function(e){
+      var id = e.currentTarget.dataset.id;
+      wx.navigateTo({ url: '../orderDetail/orderDetail?id='+id });
+    },
 });
+
+
+// -----------------------methods------------------------
+
+function getNowDate() {
+    var d = new Date();
+    var y = d.getFullYear()
+    var m = d.getMonth() + 1;
+    var d = d.getDate();
+    return y+'-'+(m<10?'0'+m:m)+'-'+(d<10?'0'+d:d);
+}
+
+function getWeekFirstDay(){     
+    var Nowdate=new Date();     
+    var WeekFirstDay=new Date(Nowdate-(Nowdate.getDay()-1)*86400000);   
+    var M=Number(WeekFirstDay.getMonth())+1;
+    var D=WeekFirstDay.getDate();
+    return WeekFirstDay.getFullYear()+"-"+(M<10?'0'+M:M)+'-'+(D<10?'0'+D:D);
+}
+
+function getWeekLastDay(){     
+    var Nowdate=new Date();     
+    var WeekFirstDay=new Date(Nowdate-(Nowdate.getDay()-1)*86400000);     
+    var WeekLastDay=new Date((WeekFirstDay/1000+6*86400)*1000);     
+    var M=Number(WeekLastDay.getMonth())+1     
+    var D=WeekLastDay.getDate();
+    return WeekLastDay.getFullYear()+"-"+(M<10?'0'+M:M)+'-'+(D<10?'0'+D:D);
+}
+
+function getMonthFirstDay(){     
+    var Nowdate=new Date();     
+    var MonthFirstDay=new Date(Nowdate.getFullYear(),Nowdate.getMonth(),1);     
+    var M=Number(MonthFirstDay.getMonth())+1;
+    var D=MonthFirstDay.getDate();
+    return MonthFirstDay.getFullYear()+"-"+(M<10?'0'+M:M)+'-'+(D<10?'0'+D:D);     
+}
+
+function getMonthLastDay(){     
+    var Nowdate=new Date();     
+    var MonthNextFirstDay=new Date(Nowdate.getFullYear(),Nowdate.getMonth()+1,1);     
+    var MonthLastDay=new Date(MonthNextFirstDay-86400000);     
+    var M=Number(MonthLastDay.getMonth())+1;
+    var D=MonthLastDay.getDate();   
+    return MonthLastDay.getFullYear()+"-"+(M<10?'0'+M:M)+'-'+(D<10?'0'+D:D);     
+}
--- pages/jujiGarden/orderDetail/orderDetail.js
+++ pages/jujiGarden/orderDetail/orderDetail.js
@@ -1,74 +1,29 @@
-// pages/jujiGarden/orderDetail/orderDetail.js
+import { jugardenService } from '../shared/service.js'
+import { constant } from '../../../utils/constant';
+import { errDialog, loading } from '../../../utils/util';
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
     productUrl: '/images/newerGetBg.png',
-    phone: '15210921650'
-
-
+    phone: '15210921650',
+    orderInfo:{}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-    wx.setNavigationBarTitle({
-      title: '订单详情',
-    })
+    wx.setNavigationBarTitle({title: '订单详情'});
+    this.getData(options.id);
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**  拨打电话  **/
-  onTelClick() {
+  onTelClick:function() {
     let self = this;
     wx.makePhoneCall({
       phoneNumber: self.data.phone
     })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
- 
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  getData:function(id){
+      jugardenService.getIncomeOrder({orderNo:id}).subscribe({
+          next: res => {
+            this.setData({orderInfo:res});
+          },
+          error: err => errDialog(err),
+          complete: () => wx.hideToast()
+      })
   }
 })

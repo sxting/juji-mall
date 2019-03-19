14ece18d61c01ffd13c35e03247f083c412f7422
Sunch <494945016@qq.com>
2019-03-19 18:36:52 +0800

    桔集支付结果

--- pages/payresult/index.js
+++ pages/payresult/index.js
@@ -1,6 +1,9 @@
 import {
   constant
 } from '../../utils/constant';
+import {
+  service
+} from '../../service';
 Page({
 
   /**
@@ -10,13 +13,25 @@ Page({
     addMoney:0,
     prepayMoney:0,
     wxPayMoney:0,
-    payUrl: 'https://juji.juniuo.com'
+    payUrl: 'https://juji.juniuo.com',
+    showDetail: false,
+    recommendList: [],
+    point: 0,
+    preBalance: 0,
+    balance: 0,
+    givingMoney: 0,
+    aMoney:0, //储值金额 不算赠送
+    zhuheAmount: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // options.orderId = '1552963656620554665079';//余额支付
+    // options.orderId = '1552989230513662818301';//微信支付
+    // options.orderId = '1552990946988705024048';//储值支付
+    // options.orderId = '1552990220020184525455';//组合支付
     if (!options.orderId) {
       wx.showModal({
         title: '系统错误',
@@ -49,10 +64,59 @@ Page({
            console.log(res);
            if(res.data.errorCode=='0'){
              this.setData({
+               zhuheAmount: res.data.data.wxPayMoney && res.data.data.wxPayMoney > 0 && res.data.data.prepayMoney > 0 ? (Number(res.data.data.prepayMoney) + Number(res.data.data.wxPayMoney)).toFixed(2) : res.data.data.prepayMoney,
+               aMoney: res.data.data.givingMoney ? Number(res.data.data.addMoney) - Number(res.data.data.givingMoney) : res.data.data.addMoney,
+               givingMoney: res.data.data.givingMoney ? res.data.data.givingMoney:0,
+               preBalance: res.data.data.prepayMoney ? (Number(res.data.data.accountDto.balance) + Number(res.data.data.prepayMoney)).toFixed(2) : res.data.data.accountDto.balance,
+               balance: res.data.data.accountDto.balance,
+               point: res.data.data.point,
                addMoney: res.data.data.addMoney?Number(res.data.data.addMoney):0,
                prepayMoney: res.data.data.prepayMoney?Number(res.data.data.prepayMoney):0,
                wxPayMoney: res.data.data.wxPayMoney?Number(res.data.data.wxPayMoney):0
              });
+             var obj = {
+                latitude: res.data.data.store.lat,
+               longitude: res.data.data.store.lng
+              }
+            service.getSelectProviderByLoc(obj).subscribe({
+              next: res2 => {
+                console.log('----------服务商信息---------');
+                console.log(res2);
+                if (res2.id) { //如果存在服务商
+                  //根据位置查询附近精选
+                  var obj2 = {
+                    // providerId: '1215434805522133',//测试
+                    providerId: res2.id,
+                    type: 'PRODUCT',
+                    sortField: 'IDX',
+                    sortOrder: 'ASC',
+                    pageNo: 1,
+                    pageSize: 4,
+                    longitude: res.data.data.store.lng,
+                    latitude: res.data.data.store.lat
+                  };
+
+                  service.getRecommendPage(obj2).subscribe({
+                    next: res3 => {
+                      console.log(res3);
+                      this.setData({
+                        recommendList: res3.list
+                      });
+                    },
+                    error: err => {
+                      console.log(err);
+                    },
+                    complete: () => wx.hideToast()
+                  });
+
+                } else { //如果不存在服务商
+                  // wx.showModal({
+                  //   title: '错误',
+                  //   content: '当前位置不存在服务商'
+                  // });
+                }
+              }
+            });
            }else{
              wx.showModal({
                title: '错误: ' + res.data.errorCode,
@@ -60,7 +124,49 @@ Page({
              })
            }
         }
-      })
+      });
+
+      // service.getSelectProviderByLoc(obj).subscribe({
+      //   next: res => {
+      //     console.log('----------服务商信息---------');
+      //     console.log(res);
+      //     if (res.id) { //如果存在服务商
+      //       //根据位置查询附近精选
+      //       var obj = {
+      //         providerId: res.id,
+      //         type: 'PRODUCT',
+      //         sortField: 'IDX',
+      //         sortOrder: 'ASC',
+      //         pageNo: 1,
+      //         pageSize: 4,
+      //         longitude: wx.getStorageSync('curLongitude'),
+      //         latitude: wx.getStorageSync('curLatitude')
+      //       };
+
+      //       service.getRecommendPage(obj).subscribe({
+      //         next: res => {
+      //           console.log(res);
+      //           this.setData({
+      //             recommendList: res.list
+      //           });
+      //         },
+      //         error: err => {
+      //           console.log(err);
+      //         },
+      //         complete: () => wx.hideToast()
+      //       });
+
+      //     } else { //如果不存在服务商
+      //       wx.showModal({
+      //         title: '错误',
+      //         content: '当前位置不存在服务商'
+      //       });
+      //     }
+      //   }
+      // });
+
+      
+
     }
   },
 
@@ -81,6 +187,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
+    // wx.setStorageSync('scene', '1011');//测试用
     let scene = wx.getStorageSync('scene');
     if (scene == '1011' || scene == '1012' || scene == '1013') {//扫描二维码场景值
       return;
@@ -89,6 +196,25 @@ Page({
         url: '/pages/index/index',
       })
     }
+  },
+  //切换详情的隐藏与显示
+  toggleDetailWrap: function(){
+    this.setData({
+      showDetail: !this.data.showDetail
+    });
+  },
+  goIndex: function(){
+    wx.switchTab({
+      url: '/pages/index/index',
+    });
+  },
+  toComDetail: function (e) {
+    var id = e.currentTarget.dataset.id;
+    var storeid = e.currentTarget.dataset.storeid;
+    console.log(id);
+    wx.navigateTo({
+      url: '/pages/comDetail/index?id=' + id + '&storeid=' + storeid
+    });
   },
 
   phoneCall: function(){

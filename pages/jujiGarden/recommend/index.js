e53214875233f3aaafa6469d8596b17a2e5a1c6f
chounan <chounannan@juniuo.com>
2019-03-20 10:47:32 +0800

    dev

--- pages/jujiGarden/recommend/index.js
+++ pages/jujiGarden/recommend/index.js
@@ -1,25 +1,71 @@
-import {service} from '../../../service';
-import {constant} from '../../../utils/constant';
-import {errDialog,loading} from '../../../utils/util';
-var app = getApp();
-Page({
-    data: {
-        recommendlist: [{imgIds:['','','','','','']},{imgIds:['','','','','','']}],
-        constant: constant,
-        isShowNodata: false,
-        curTabIndex:1
-    },
-    getData: function() {
+import { service } from '../../../service';
+import { constant } from '../../../utils/constant';
+import { errDialog, loading } from '../../../utils/util';
 
-    },
-    switchTab: function(e) {
-        this.setData({curTabIndex:e.currentTarget.dataset.index});
-        this.getData();
-    },
-    onLoad: function(options) {
-        wx.setNavigationBarTitle({
-            title: '推广素材',
-        });
-        this.getData();
-    }
+
+Page({
+  data: {
+    recommendlist: [{imgIds:['','','','','','']}],
+    picId: '25R9F7zL2Dhr',
+    constant: constant,
+    isShowNodata: false,
+    curTabIndex:1
+  },
+  onLoad: function(options) {
+      wx.setNavigationBarTitle({
+          title: '推广素材',
+      });
+  },
+
+  switchTab: function (e) {
+    this.setData({ curTabIndex: e.currentTarget.dataset.index });
+  },
+
+  /**
+   * 生成图文
+   */
+  showImageTextToShare(){
+    wx.showLoading({ title: '生成图片...' });
+    wx.downloadFile({
+      url: constant.basePicUrl + this.data.picId + '/resize_240_240/mode_fill',
+      success: (res) => {
+        if (res.statusCode === 200) {
+         
+        } else {
+          wx.hideLoading();
+        }
+      }
+    });
+  },
+
+  /**
+   * 分享给朋友
+   **/ 
+  shareToFriend(){
+
+   },
+
+  /**
+  * 分享到朋友圈
+  **/
+  shareToWechatCircles() {
+
+  },
+
+  /**
+   * 保存素材
+   **/
+  saveImagesToPhone() {
+
+  },
+
 })
+
+
+// get推广素材list信息
+function getProductListInfor(){
+  let self = this;
+  let data = {
+
+  }
+}

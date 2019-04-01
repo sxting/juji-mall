b833ee99313586ec0f24f6189b1e80eb7c5d81eb
chounan <chounannan@juniuo.com>
2019-04-01 15:19:04 +0800

    css about juyuan

--- utils/constant.js
+++ utils/constant.js
@@ -1,5 +1,5 @@
 var constant = {
-  TPLID: 'test1.0.03',//应用版本
+  TPLID: '1.0.04',//应用版本
   APPID: 'wxb03b0ab93bb60ecc',//小程序wxid
   apiUrl: 'https://c.juniuo.com/shopping',//接口前缀 http://juzi-dev.juniuo.cn
   // apiUrl: 'http://juzi-dev.juniuo.cn',//测试接口前缀 

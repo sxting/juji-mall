f0b99885406d9bb40f741166da8bf170335d7405
wanghongguo <wanghongguo@juniuo.com>
2019-04-03 18:02:09 +0800

    ti

--- pages/comDetail/index.js
+++ pages/comDetail/index.js
@@ -559,9 +559,9 @@ Page({
                     this.setData({erwmImg:res1.tempFilePath});
                     var info = this.data.productInfo;
                     wx.hideLoading();
-                    var point = info.point==0?'':info.point+'桔子';
-                    var price = info.price==0?'':Number(info.price/100).toFixed(2)+'元';
-                    var link = info.price!=0&&info.point!=0?'+':'';
+                    var point = info.point==null||info.point==0?'':info.point+'桔子';
+                    var price = info.price==null||info.price==0?'':Number(info.price/100).toFixed(2)+'元';
+                    var link = (info.price!=null&&info.point!=0)&&(info.point!=null&&info.point!=0)?'+':'';
                     var price1 = point + link + price;
                     var name = info.productName;
                     var price2 = Number(info.originalPrice / 100).toFixed(2) + '元';

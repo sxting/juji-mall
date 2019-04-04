b112ca0827f45989974f9256e66c2d652bc7ea55
wanghongguo <wanghongguo@juniuo.com>
2019-04-04 19:01:03 +0800

    ti

--- pages/jujiGarden/recommend/index.js
+++ pages/jujiGarden/recommend/index.js
@@ -67,7 +67,7 @@ Page({
             this.closeModal();
             return {
                 title: JSON.parse(wx.getStorageSync('userinfo')).nickName + '分享给您一个心动商品，快来一起体验吧！',
-                path: '/pages/login/index?pagetype=4&pid=' + this.data.productId + '&storeid=&sceneid='+this.data.sceneId,
+                path: '/pages/login/index?pagetype=4&pid=' + this.data.productId + '&storeid=&sceneid='+this.data.sceneId + '&invitecode='+wx.getStorageSync('inviteCode'),
                 imageUrl: constant.basePicUrl + this.data.productInfo.picId + '/resize_360_360/mode_fill'
             }
         }

41dd66e26a47b81104067d6234b446dc7b171ea7
chounan <chounannan@juniuo.com>
2019-04-08 15:00:20 +0800

    change debug about fenye

--- pages/activities/project-list/index.js
+++ pages/activities/project-list/index.js
@@ -5,7 +5,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    sceneType: 'PINTUAN',
+    sceneType: 'PINTUAN',//查看进入的是什么场景类型
     productList: ['','',''],
   
   },
@@ -14,6 +14,12 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    wx.setNavigationBarTitle({ 
+      title: options.sceneType == 'PINTUAN'? '拼团列表': '砍价列表'
+    });
+    this.setData({
+      sceneType: options.sceneType
+    })
 
   },
 
@@ -36,5 +42,21 @@ Page({
    */
   onShareAppMessage: function () {
 
-  }
+  },
+
+  /**
+   * 跳转我的砍价或者我的拼团页面
+   */
+  switchToOrderListPage: function(){
+
+  },
+
+  /**
+   * 到项目详情
+   */
+  checkProductDetail: function(e){
+    wx.navigateTo({
+      url: '/pages/activities/project-detail/index?sceneType=' + this.data.sceneType + '&productId=' + e.currentTarget.dataset.productid
+    });
+  },
 })

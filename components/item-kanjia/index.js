465022a7a95e6a181e35ae0f87a3493834872d75
sxt <994747304@qq.com>
2019-04-09 15:05:09 +0800

    kanjia

--- components/item-kanjia/index.js
+++ components/item-kanjia/index.js
@@ -15,17 +15,14 @@ Component({
     showAlert1: false,
     showAlert2: false,
     status: 'ing', //未开始 init，砍价中 ing，砍价失败 fail，砍价成功 success
-    shared: false,
+    shared: true,
     self: true, //是否活动发起者
-    help: false, //是否已帮砍
+    help: true, //是否已帮砍
     hours: 23,
     minites: 20,
     seconds: 58,
   },
 
-  /**
-   * 组件的方法列表
-   */
   methods: {
 
   }

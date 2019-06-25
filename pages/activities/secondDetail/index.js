4bfd1cecc05f37fc9e9a04ca9a8ac891112c93c6
wanghongguo <wanghongguo@juniuo.com>
2019-06-25 21:16:31 +0800

    ti

--- pages/activities/secondDetail/index.js
+++ pages/activities/secondDetail/index.js
@@ -39,6 +39,19 @@ Page({
         phone:''
     },
     onLoad: function(options) {
+        const updateManager = wx.getUpdateManager();
+        updateManager.onUpdateReady(function() {
+            wx.showModal({
+                title: '更新提示',
+                content: '新版本已经准备好，是否重启应用？',
+                success(res) {
+                    if (res.confirm) {
+                        updateManager.applyUpdate()
+                    }
+                }
+            })
+        });
+        
         wx.setNavigationBarTitle({ title: '商品详情' });
         var inviteCode = wx.getStorageSync('inviteCode');
         this.setData({

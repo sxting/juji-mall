096c0f17b8a04cd6db989b39b487a7b106eecf61
wanghongguo <wanghongguo@xiaoxiannvdeiMac.local>
2019-08-13 18:21:59 +0800

    ti

--- pages/seedDetail/index.js
+++ pages/seedDetail/index.js
@@ -2,50 +2,33 @@ var util = require('../../utils/util.js');
 import { constant } from '../../utils/constant';
 import { errDialog, loading } from '../../utils/util';
 import { service } from '../../service.js';
+var WxParse = require('../../wxParse/wxParse.js');
+
+
+
+
 
 Page({
     data: {
         nvabarData: { showCapsule: 1, title: '种草'},
-        providerId:'',
-        productList: [],
-        isShowNodata: false,
-        pageNo: 1,
-        ifBottom: false
+        tweetInfo: {},
+        content:{}
     },
     onLoad: function(options) {
-        this.setData({providerId: wx.getStorageSync('providerId')});
         this.getData(options.id); //获取活动列表
-    },
-    onReachBottom: function() {
-        if (this.data.ifBottom) { //已经到底部了
-            return;
-        } else {
-            this.setData({
-                pageNo: this.data.pageNo + 1
-            })
-            this.getData();
-        }
     },
     getData: function(id) {
         service.tweetDetail({tweetsId:id}).subscribe({
             next: res => {
                 this.setData({
-
-                });
+                  tweetInfo:res,w
+                  content:WxParse.wxParse('content', 'html', res.html, this)
+                })
             },
             error: err => {},
             complete: () => wx.hideToast()
         });
     },
-    //跳转到商品详情
-    toComDetail: function(e) {
-        var id = e.currentTarget.dataset.id;
-        var storeid = e.currentTarget.dataset.storeid;
-        wx.navigateTo({
-            url: '/pages/comDetail/index?share=0&id=' + id + '&storeid=' + storeid
-        });
-    },
-    // 分享
     toComDetailAndShare: function(e) {
         var id = e.currentTarget.dataset.id;
         var storeid = e.currentTarget.dataset.storeid;

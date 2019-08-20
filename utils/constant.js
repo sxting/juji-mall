aff0a464213a6073bb42ba6bfd88b2f6a18105b8
wanghongguo <wanghongguo@wiseeasy.com>
2019-08-20 16:45:43 +0800

    t

--- utils/constant.js
+++ utils/constant.js
@@ -1,8 +1,8 @@
 var constant = {
   TPLID: '1.0.07',//应用版本
   APPID: 'wxb03b0ab93bb60ecc',//小程序wxid
-//   apiUrl: 'https://c.juniuo.com/shopping', //mall
-  apiUrl: 'https://upic-test.juniuo.com',//测试环境
+  apiUrl: 'https://c.juniuo.com/wxapp', //mall //shopping
+  // apiUrl: 'https://upic-test.juniuo.com',//测试环境
   //jujipayUrl: 'https://juji.juniuo.com', //测试 https://juji-dev.juniuo.com
   uploadUrl: 'https://juji.juniuo.com',
   basePicUrl: 'https://upic.juniuo.com/file/picture/',//图片前缀

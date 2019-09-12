68a74e2a4b97ed3c8dac8823b2c500d7b443293e
xiaoting <994747304@qq.com>
2019-09-12 16:47:39 +0800

    授权完成后调登录接口

--- pages/authorize/index.js
+++ pages/authorize/index.js
@@ -37,7 +37,15 @@ Page({
         if (e.detail.userInfo) {
             console.log('点击授权按钮');
             wx.setStorageSync('rawData', e.detail.rawData);
-            this.nextPage();
+
+            var obj = {
+                rawData: e.detail.rawData,
+                inviteCode: this.data.inviteCode,
+                scene: this.data.scene
+            };
+            this.login(obj).then(() => {
+                this.nextPage();
+            });
         }
     },
 
@@ -58,9 +66,69 @@ Page({
             wx.redirectTo({
                 url: `/pages/payOrder/index?${str}`,
             })
-            // paytype=3&id=2019072916415006016069712&storeid=111563783233186948&sceneid=&inviteCode=&skuId=cYZggNDm&smId=1147&buyMember=undefined
-
-            // paytype=3&id=2019082211035731338375102&storeid=111552548538131232&sceneid=&inviteCode=&skuId=2r85k4KH&smId=1415&buyMember=1
         }
+    },
+
+    login: function (obj) {
+        return new Promise(function (resolve1, reject1) {
+            wx.login({
+                success: res => {
+                    console.log('code: ' + res.code);
+                    console.log(constant.APPID);
+                    resolve1(res.code);
+                }
+            });
+        }).then(function (code) {
+            return new Promise(function (resolve2, reject2) {
+                console.log("登录请求inviteCode=" + obj.inviteCode);
+                console.log("登录请求scene=" + obj.scene);
+                wx.request({
+                    url: constant.apiUrl + '/user/login.json',
+                    method: 'GET',
+                    data: {
+                        code: code,
+                        appId: constant.APPID,
+                        isMock: false, //测试标记
+                        inviteCode: obj.inviteCode,
+                        rawData: obj.rawData,
+                        sceneId: obj.scene
+                    },
+                    header: {
+                        'content-type': 'application/json',
+                    },
+                    success: (res1) => {
+                        console.log(res1);
+                        if (res1.data.errorCode == '200') {
+                            console.log('登录成功，拿到token');
+                            wx.setStorageSync('token', res1.data.data.token);
+                            wx.setStorageSync('openid', res1.data.data.openId);
+                            wx.setStorageSync('inviteCode', res1.data.data.inviteCode);
+                            wx.setStorageSync('userinfo', JSON.stringify(res1.data.data));
+                            wx.setStorageSync('distributorRole', res1.data.data.distributorRole);
+                            wx.setStorageSync('member', res1.data.data.member);
+                            wx.setStorageSync('level', res1.data.data.level);
+                            wx.setStorageSync('memberDays', res1.data.data.memberDays);
+                            wx.setStorageSync('memberExpireTime', res1.data.data.memberExpireTime);
+                            wx.setStorageSync('nickName', res1.data.data.nickName);
+                            wx.setStorageSync('avatar', res1.data.data.avatar);
+                            wx.setStorageSync('memberInviteCode', res1.data.data.memberInviteCode)
+
+                            resolve2();
+                        } else {
+                            reject2('登录失败，错误码:' + res1.data.errorCode + ' 返回错误: ' + res1.data.errorInfo);
+                        }
+                    },
+                    fail: (err) => {
+                        reject2(err.errMsg);
+                    }
+                });
+            });
+        }).catch(function (err) {
+            console.log(err);
+            wx.showModal({
+                title: '错误',
+                content: err
+            });
+        });
     }
 })
